ed78dad122bc7ec125a3936990aa8d7b93b2f271
zhuifeng1017@gmail.com <zhuifeng1017@gmail.com>
2013-09-18 17:39:10 +0800

    0000000

---
+++ test/Socks5Proxy.cpp
@@ -0,0 +1,9 @@
+#include "Socks5Proxy.h"
+
+CSocks5Proxy::CSocks5Proxy(void)
+{
+}
+
+CSocks5Proxy::~CSocks5Proxy(void)
+{
+}
---
+++ test/Socks5Proxy.h
@@ -0,0 +1,12 @@
+#ifndef __PocoTest__CSocks5Proxy__
+#define __PocoTest__CSocks5Proxy__
+
+class CSocks5Proxy
+{
+public:
+	CSocks5Proxy(void);
+	virtual ~CSocks5Proxy(void);
+};
+
+
+#endif /* defined(__PocoTest__CSocks5Proxy__) */
